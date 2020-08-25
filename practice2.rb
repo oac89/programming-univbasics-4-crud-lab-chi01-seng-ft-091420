dc023bd81eda112e82581d83edfaa6997abc80e1
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 16:09:01 +0000

    Automatically backed up by Learn

---
+++ practice2.rb
@@ -0,0 +1,6 @@
+def add_element_to_end_of_array(array , element)
+  puts "#{array}"
+  puts add_element_to_end_of_array.push(#{element)  
+
+end 
+add_element_to_end_of_array("['red', 'blue']" , "yellow" )
