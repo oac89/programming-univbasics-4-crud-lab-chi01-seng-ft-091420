855703a97e4ee6570248d044393a2d2fea82d390
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 16:28:36 +0000

    Automatically backed up by Learn

--- practice2.rb
+++ practice2.rb
@@ -1,6 +1,6 @@
-def add_element_to_end \n  ["red", "blue"]
-add_element_to_end << "green"
-puts add_element_to_end
+def add_element_to_end["red", "blue"]
+  add_element_to_end << "green"
+  puts add_element_to_end
 end
   
 
