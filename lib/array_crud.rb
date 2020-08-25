6403a46d91ada43f9b786adc0cb0d27f299a512e
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 17:35:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,46 @@
-def create_an_empty_array
-  
-end
+def create_an_empty_array 
+   []
+end   
+def create_an_array  
+   ["blue", "green", "red", "orange"]
+end 
 
-def create_an_array
-  
-end
+def add_element_to_end_of_array(array , element)
+      ##colors_arr = ["red", "blue"]
+      array << element
+end 
 
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array , element)
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
+  element = array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  element = array.shift 
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array , element)
+  array[element]
   
-end
+end 
 
 def retrieve_first_element_from_array(array)
-  
+    array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
+def update_element_from_index(array, index, element)
+  array[index] = element
 
-end
+end   
+
+
+
---
+++ practice2.rb
@@ -0,0 +1,9 @@
+def add_element_to_end["red", "blue"]
+  add_element_to_end << "green"
+  puts add_element_to_end
+end
+  
+
+ 
+
+
