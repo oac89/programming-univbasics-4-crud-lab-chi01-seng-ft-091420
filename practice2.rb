413530cc622737bea275c818df2f6c3bc617cc75
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 16:15:27 +0000

    Automatically backed up by Learn

--- practice2.rb
+++ practice2.rb
@@ -1,6 +1,6 @@
-def add_element_to_end(array, element)
+def add_element_to_end (array, element)
   puts "array"
   puts add_element_to_end_of_array.push("element")  
 
 end 
-add_element_to_end("['red', 'blue']" , "yellow")
+add_element_to_end ("['red', 'blue']" , "yellow")
