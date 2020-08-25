c1c7770dae2e8c801c24028f5073256fe969e71b
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 15:44:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,14 @@
-def create_an_empty_array
-  
-end
+def create_an_empty_array 
+   []
+end   
+def create_an_array  
+   ["blue", "green", "red", "orange"]
+end 
 
-def create_an_array
-  
-end
+def add_element_to_end_of_array(array , element) 
+  puts #{array}   
+  puts add_element_to_end_of_array.push(#{element)  
 
-def add_element_to_end_of_array(array, element)
-  
-end
+end 
+add_element_to_end_of_array("['red', 'blue']" , "yellow" )
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
-end
---
+++ practice.rb
@@ -0,0 +1,56 @@
+def greeting(name = 'neighbor')
+  puts "Hello, #{name}!"
+end
+ greeting
+ 
+ def add_element_to_end_of_array('green' 'red')
+   puts `add_element_to_end_of_array`
+end
+add_element_to_end_of_array
+
+
+2.6.1 :001 > colors_if =["red", "blue", "yellow"]
+ => ["red", "blue", "yellow"]
+2.6.1 :002 > colors_if.push"maroon"
+ => ["red", "blue", "yellow", "maroon"]
+ 
+ 
+ def add_element_to_start_of_array(array, element) 
+  #['blue', 'green'] 
+  #add_an_element_to_start_of_array.unshift_'tan'  
+  
+#end
+
+#def remove_element_from_end_of_array(array)
+  #['blue', 'green']
+  #orange_element = remove_element_from_end_of_array.pop 
+
+#end
+
+#def remove_element_from_start_of_array(array)
+  #['blue'] 
+  #blue = remove_element_from_start_of_array.shift
+  
+#end
+
+#def retrieve_element_from_index(array, index_number)
+    #['blue', 'green']
+    retrieve_element_from_index[2]
+#end
+
+#def retrieve_first_element_from_array(array)
+  #['blue']
+    retrieve_element_from_index[0]
+  
+#end
+
+#def retrieve_last_element_from_array(array)
+  [#{color1, #{color2}]
+#  retrieve_element_from_index[-1]
+#end
+
+#def update_element_from_index(array, index_number, element)
+ # ['green', 'red', 'oragne']
+  #retrieve_element_from_index[1]
+  #retrieve_element_from_index[1] = 'pink'  
+#end
